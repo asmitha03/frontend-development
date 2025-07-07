f017563dc4e42a22ed0824238cbda3da034533b3
Asmitha M <144476066+asmitha03@users.noreply.github.com>
2025-07-07 14:29:20 +0530

    first commit

---
+++ calculator_app/script.js
@@ -0,0 +1,14 @@
+function addToDisplay(value)
+{
+   document.getElementById("display").value += value;
+}
+function calculate()
+{
+    let input = document.getElementById("display").value;
+    let result = eval(input);
+    document.getElementById("display").value = result;
+}
+function clearDisplay()
+{
+    document.getElementById("display").value = "";
+}
